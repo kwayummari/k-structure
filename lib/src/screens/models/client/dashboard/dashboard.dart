35a8efc808eaccf829a6727f64701170fc5e95a4
Kwayu Mmari <mmaridenis@gmail.com>
2024-01-22 11:22:00 +0300

    removed geolocator dependencies

--- lib/src/screens/models/client/dashboard/dashboard.dart
+++ lib/src/screens/models/client/dashboard/dashboard.dart
@@ -5,7 +5,6 @@ import 'package:kstructure/src/utils/routes/route-names.dart';
 import 'package:kstructure/src/widgets/app_base_screen.dart';
 import 'package:kstructure/src/widgets/app_input_text.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:kstructure/src/widgets/app_text.dart';
 
 class dashboard extends StatefulWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_macos
-import geolocator_apple
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
@@ -14,7 +13,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
