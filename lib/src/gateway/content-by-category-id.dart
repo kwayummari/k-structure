b88d0ea1829ff0d5b59b09efe99cfc15c1d2755f
Kwayu Mmari <mmaridenis@gmail.com>
2024-01-22 12:51:57 +0300

    fixing contentbbucategoryid gateway file

--- lib/src/gateway/content-by-category-id.dart
+++ lib/src/gateway/content-by-category-id.dart
@@ -6,8 +6,11 @@ import 'package:flutter/material.dart';
 class categoriesServices {
   Api api = Api();
 
-  Future getCategories(BuildContext context) async {
-    final response = await api.get(context, 'categories');
+  Future getCategories(BuildContext context, int categoryId) async {
+    Map<String, dynamic> data = {
+      'phone_number': categoryId.toString(),
+    };
+    final response = await api.post(context, 'categories', data);
     final decodedResponse = jsonDecode(response.body);
     return decodedResponse;
   }
