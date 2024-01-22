2be1dafc8075e40fc3780d2a93e1f12deb679441
Kwayu Mmari <mmaridenis@gmail.com>
2024-01-22 12:46:58 +0300

    created contentbbucategoryid gateway file

---
+++ lib/src/gateway/content-by-category-id.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:kstructure/src/api/apis.dart';
+import 'package:flutter/material.dart';
+
+class categoriesServices {
+  Api api = Api();
+
+  Future getCategories(BuildContext context) async {
+    final response = await api.get(context, 'categories');
+    final decodedResponse = jsonDecode(response.body);
+    return decodedResponse;
+  }
+}
