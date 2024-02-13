4daf527f079f45d47c7ca1c4380a8afbd6e20c38
Kwayu Mmari <mmaridenis@gmail.com>
2024-02-13 15:02:18 +0300

    fixed

--- lib/src/widgets/star_widget.dart
+++ lib/src/widgets/star_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:kstructure/src/utils/app_const.dart';
 
 class StarRating extends StatelessWidget {
   final String numberOfStars;
@@ -16,8 +15,8 @@ class StarRating extends StatelessWidget {
         color: Colors.amber,
       ),
       itemCount: 5,
-      itemSize: 50.0,
-      direction: Axis.vertical,
+      itemSize: 30.0,
+      direction: Axis.horizontal,
     );
   }
 }
