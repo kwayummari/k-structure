8f3d759710c2770adc06592c8cb645b4f6280c56
Kwayu Mmari <mmaridenis@gmail.com>
2024-02-15 17:00:44 +0300

    fixed content by id file

--- lib/src/widgets/app_course_details.dart
+++ lib/src/widgets/app_course_details.dart
@@ -1,27 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:kstructure/src/utils/app_const.dart';
+import 'package:kstructure/src/widgets/app_text.dart';
 
 class AppCourseDetails extends StatelessWidget {
-  final Widget child;
-  final Color color;
-  final double? elevation;
-  final double border;
+  final Icon icon;
+  final String text;
 
   const AppCourseDetails(
-      {Key? key, required this.child, this.elevation, required this.color, required this.border})
+      {Key? key, required this.icon, required this.text})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(border),
-      ),
-      elevation: elevation ?? 1,
-      color: color,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 0, 16, 24),
-        child: child,
-      ),
-    );
+    return Row(
+                    children: [
+                      icon,
+                      SizedBox(
+                        width: 20,
+                      ),
+                      AppText(
+                        txt: text,
+                        size: 20,
+                        weight: FontWeight.w600,
+                        color: AppConst.black,
+                      ),
+                    ],
+                  );
   }
 }
