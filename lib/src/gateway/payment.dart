ed8048e5a1e62584729494bd1e9c28db1266cfec
Kwayu Mmari <mmaridenis@gmail.com>
2024-04-22 01:40:15 +0300

    fixed payment ui

---
+++ lib/src/gateway/payment.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../api/apis.dart';
+import '../provider/login-provider.dart';
+import '../utils/routes/route-names.dart';
+import '../widgets/app_snackbar.dart';
+
+class paymentService{
+  final Api api = Api();
+
+  Future<void> payment(
+      BuildContext context, String phoneNumber) async {
+    final myProvider = Provider.of<MyProvider>(context, listen: false);
+    myProvider.updateLoging(!myProvider.myLoging);
+    final prefs = await SharedPreferences.getInstance();
+      var user_id = await prefs.getString('id');
+    Map<String, dynamic> data = {
+      'phone_number': phoneNumber,
+      'user_id': user_id
+    };
+
+    final response = await api.post(context, 'payment', data);
+    final newResponse = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      myProvider.updateLoging(!myProvider.myLoging);
+      AppSnackbar(
+        isError: false,
+        response: newResponse['message'],
+      ).show(context);
+      AppSnackbar(
+        isError: false,
+        response: newResponse['message'],
+      ).show(context);
+    } else {
+      myProvider.updateLoging(!myProvider.myLoging);
+      AppSnackbar(
+        isError: true,
+        response: newResponse['message'],
+      ).show(context);
+    }
+  }
+}
--- lib/src/screens/models/payment/payment.dart
+++ lib/src/screens/models/payment/payment.dart
@@ -2,9 +2,15 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:kstructure/src/gateway/payment.dart';
+import 'package:kstructure/src/provider/login-provider.dart';
 import 'package:kstructure/src/utils/app_const.dart';
 import 'package:kstructure/src/widgets/app_base_screen.dart';
+import 'package:kstructure/src/widgets/app_button.dart';
+import 'package:kstructure/src/widgets/app_input_text.dart';
 import 'package:kstructure/src/widgets/app_text.dart';
+import 'package:provider/provider.dart';
 
 class Payment extends StatefulWidget {
   var id;
@@ -17,27 +23,17 @@ class Payment extends StatefulWidget {
 }
 
 class _PaymentState extends State<Payment> {
-  // void fetchData() async {
-  //   contentsByCategoriesServices ContentsServices =
-  //       contentsByCategoriesServices();
-  //   final details =
-  //       await ContentsServices.getContentsDetails(context, widget.id);
-  //   final contentVideos =
-  //       await ContentsServices.getContentsVideos(context, widget.id);
-  //   setState(() {
-  //     contentDetails = details['details'] ?? [];
-  //     videos = contentVideos['videos'] ?? [];
-  //   });
-  // }
+  TextEditingController phone_number = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     super.initState();
-    // fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
+    final myProvider = Provider.of<MyProvider>(context);
     return AppBaseScreen(
         appBar: AppBar(
           title: AppText(
@@ -51,39 +47,76 @@ class _PaymentState extends State<Payment> {
         isvisible: false,
         backgroundImage: false,
         backgroundAuth: false,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Divider(
-              color: AppConst.black,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            AppText(
-              txt:
-                  'You will be enrolled to this course after you successfully complete payment',
-              size: 23,
-              color: AppConst.black,
-              weight: FontWeight.w600,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            AppText(
-              txt: 'Choose Payment',
-              size: 23,
-              color: AppConst.black,
-              weight: FontWeight.w600,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Image.asset('assets/mobiles.png'),
-            SizedBox(
-              height: 20,
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Divider(
+                color: AppConst.black,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              AppText(
+                txt:
+                    'You will be enrolled to this course after you successfully complete payment',
+                size: 23,
+                color: AppConst.black,
+                weight: FontWeight.w600,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              AppText(
+                txt: 'Choose Payment',
+                size: 23,
+                color: AppConst.black,
+                weight: FontWeight.w600,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Image.asset('assets/mobiles.png'),
+              SizedBox(
+                height: 20,
+              ),
+              AppInputText(
+                textsColor: AppConst.black,
+                textfieldcontroller: phone_number,
+                ispassword: false,
+                fillcolor: AppConst.white,
+                label: 'Enter Phone Number',
+                obscure: false,
+                icon: Icon(
+                  Icons.phone_android,
+                  color: AppConst.black,
+                ),
+                isemail: false,
+                isPhone: true,
+              ),
+              myProvider.myLoging == true
+                  ? SpinKitCircle(
+                      color: AppConst.primary,
+                    )
+                  : Container(
+                      width: 350,
+                      height: 55,
+                      child: AppButton(
+                        onPress: () {
+                          if (!_formKey.currentState!.validate()) {
+                            return;
+                          }
+                          paymentService().payment(context, phone_number.text);
+                        },
+                        label: 'LOGIN',
+                        borderRadius: 5,
+                        textColor: AppConst.white,
+                        bcolor: AppConst.primary,
+                      ),
+                    ),
+            ],
+          ),
         ));
   }
 }
